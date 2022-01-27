d18d89288a28eed42ff9fb897da51741ffd1a407
Denis Potapov <denipotapo@yandex.ru>
2022-01-28 02:43:17 +0300

    try tg auth

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import TelegramLoginButton from 'react-telegram-login';
 
 function App() {
+  const handleTelegramResponse = (response) => {
+    console.log(response);
+  };
   return (
     <div className="App">
-      <header className="App-header">app header</header>
+      <header className="App-header">yo niga</header>
+      <TelegramLoginButton
+        dataOnauth={handleTelegramResponse}
+        botName="secretapplogin_bot"
+      />
     </div>
   );
 }
