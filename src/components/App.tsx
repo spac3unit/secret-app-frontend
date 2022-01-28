563867b3a65c59ccf6717b2bc0bffc47adca406e
Denis Potapov <denipotapo@yandex.ru>
2022-01-28 03:24:17 +0300

    add chakra, fix styles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Box } from '@chakra-ui/react';
+import { Flex, Spacer, Container } from '@chakra-ui/react';
+
 import TelegramLoginButton from 'react-telegram-login';
 
 function App() {
@@ -7,11 +10,21 @@ function App() {
   };
   return (
     <div className="App">
-      <header className="App-header">yo niga</header>
-      <TelegramLoginButton
-        dataOnauth={handleTelegramResponse}
-        botName="secretapplogin_bot"
-      />
+      <Container maxW="container.xl">
+        <Flex bg="black" w="100%" p={4} color="white">
+          <Box p="4">SecretApp</Box>
+          <Spacer />
+          <Box p="4" color="white">
+            <TelegramLoginButton
+              dataOnauth={handleTelegramResponse}
+              botName="secretapplogin_bot"
+              buttonSize="medium"
+              lang="en"
+              usePic="false"
+            />
+          </Box>
+        </Flex>
+      </Container>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+
 import './index.css';
 import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
